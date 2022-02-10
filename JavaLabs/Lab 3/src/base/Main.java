4222a6af4a701169e1b3d736f61c977b95f0d518
Youssef-aly1 <Youssef.Aly1@marist.edu>
2022-02-10 17:01:08 -0500

    Lab 3
    Finished the Hello world portion of the lab, still have yet to finish the "if else" portion

--- JavaLabs/Lab 3/src/base/Main.java
+++ JavaLabs/Lab 3/src/base/Main.java
@@ -14,9 +14,29 @@ public class Main {
      */
     public static void main(String[] args) {
         // Make an if statement that triggers a print or println statement
+        if (true) {
+            System.out.println("yo.");
+        }
         // Make an if, else statement where the else statement triggers a print or println statement
+        if (true) {
+            System.out.println("hello");
+        } else{
+            System.out.println("this is java");
+        }
         // Make an if, else if, else statement where the else if statement triggers a print or println statement
+        if (true) {
+            System.out.println("this is true");
+        } else if(!true){
+            System.out.println("This is false");
+        }
         // Make 2 variables and use them in an if else conditional print from one of the sections
+        boolean skill = true;
+        boolean aim = false;
+        if (skill){
+            System.out.println("You have skill");
+        } else{
+            System.out.println("You don't have skill");
+        }
         // Make an if statement using 2 variables and an AND(&&) statement that triggers a print or println statement
         // Make an if statement using 2 variables and an OR(||) statement that triggers a print or println statement
     }
